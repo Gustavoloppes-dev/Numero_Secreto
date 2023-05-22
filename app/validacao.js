aedc588e0e221fb9f787cd91cb4bd6af5c31ebaf
Gustavoloppes-dev <gustavoloppes.dev@gmail.com>
2023-05-22 14:11:07 -0300

    Concluido

--- app/validacao.js
+++ app/validacao.js
@@ -1,5 +1,5 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
-    const numero = +chute;
+    const numero =+chute;
 
     if(chuteForInvalido(numero)) {
         elementoChute.innerHTML += "<div>valor invalido</div>"
@@ -15,7 +15,10 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     if(numero === numeroSecreto) {
         document.body.innerHTML = 
         `<h2>Você acertou!</h2>
-        <h3>O número secreto era ${numeroSecreto}.</h3>`
+        <h3>O número secreto era ${numeroSecreto}.</h3>
+        
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente!</button>
+        `
     }else if(numero > numeroSecreto) {
         elementoChute.innerHTML += `
         <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
@@ -35,3 +38,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', (e) => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
